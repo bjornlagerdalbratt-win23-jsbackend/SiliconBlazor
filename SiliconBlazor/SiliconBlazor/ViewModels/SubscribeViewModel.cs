30ab452e07e20509b66c4840dcf0adba839b3451
Helga Reesalu <helga.reesalu@gmail.com>
2024-05-16 11:35:45 +0200

    Added components to the home page + added styles

---
+++ SiliconBlazor/SiliconBlazor/ViewModels/SubscribeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiliconBlazor.ViewModels;
+
+public class SubscribeViewModel
+{
+    [EmailAddress]
+    [Required(ErrorMessage = "You must provide an email address")]
+    [Display(Name = "Email address", Prompt = "Your Email")]
+    public string Email { get; set; } = null!;
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+}
