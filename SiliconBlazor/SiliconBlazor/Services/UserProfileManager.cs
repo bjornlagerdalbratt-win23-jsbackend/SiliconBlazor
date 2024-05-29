8eb379966665370f5e6f8fd290520d7f07cbc0d2
Rebecca <rebeccalagerr@outlook.com>
2024-05-29 22:41:55 +0200

    2024-05-29-22-41

--- SiliconBlazor/SiliconBlazor/Services/UserProfileManager.cs
+++ SiliconBlazor/SiliconBlazor/Services/UserProfileManager.cs
@@ -7,11 +7,11 @@ public class UserProfileManager(ApplicationDbContext context)
 {
     private readonly ApplicationDbContext _context = context;
 
-    public async Task<UserProfile> GetUserProfileAsync(string id)
-    {
-        var userProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.Id == id);
-        return userProfile!;
-    }
+    //public async Task<UserProfile> GetUserProfileAsync(string id)
+    //{
+    //    //var userProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.Id == id);
+    //    //return userProfile!;
+    //}
 
     
 }
---
+++ SiliconBlazor/SiliconBlazor/Services/UserService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SiliconBlazor.Data;
+
+namespace SiliconBlazor.Services;
+
+public class UserService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+    public UserService(UserManager<ApplicationUser> userManager, AuthenticationStateProvider authenticationStateProvider)
+    {
+        _userManager = userManager;
+        _authenticationStateProvider = authenticationStateProvider;
+    }
+
+
+    public async Task<ApplicationUser> GetUserAsync()
+    {
+        var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity == null || !user.Identity.IsAuthenticated)
+        {
+            return null!;
+        }
+
+        var currentUser = await _userManager.GetUserAsync(user);
+
+        return currentUser ?? null!;
+    }
+
+    //private readonly UserManager<ApplicationUser> _userManager;
+    //private readonly AuthenticationStateProvider _authenticationStateProvider;
+    //private readonly ApplicationDbContext _context;
+
+    //public UserService(UserManager<ApplicationUser> userManager, AuthenticationStateProvider authenticationStateProvider, ApplicationDbContext context)
+    //{
+    //    _userManager = userManager;
+    //    _authenticationStateProvider = authenticationStateProvider;
+    //    _context = context;
+    //}
+
+    //public async Task<ApplicationUser> GetUserAsync()
+    //{
+    //    var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+    //    var user = authState.User;
+
+    //    if (user.Identity == null || !user.Identity.IsAuthenticated)
+    //    {
+    //        return null!;
+    //    }
+
+    //    var currentUser = await _userManager.GetUserAsync(user);
+
+    //    if (currentUser != null && currentUser.UserProfileId != null)
+    //    {
+    //        currentUser.UserProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.Id == currentUser.UserProfileId);
+    //    }
+
+    //    return currentUser ?? null!;
+    //}
+
+
+}
