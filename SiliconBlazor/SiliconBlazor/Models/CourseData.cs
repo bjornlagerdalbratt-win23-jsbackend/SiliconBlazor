86c84daa2cf472ddac3d4a589dd0fa44c8355748
Helga Reesalu <helga.reesalu@gmail.com>
2024-05-29 23:29:00 +0200

    added courses page - errors

---
+++ SiliconBlazor/SiliconBlazor/Models/CourseData.cs
@@ -0,0 +1,48 @@
+﻿namespace SiliconBlazor.Models;
+
+public class CourseData
+{
+    public string Id { get; set; } = null!;
+    public bool IsBestSeller { get; set; }
+    public bool IsDigital { get; set; }
+    public string? Title { get; set; }
+    public string? Ingress { get; set; }
+    public decimal StarRating { get; set; }
+    public string? Reviews { get; set; }
+    public string? ImageHeaderUri { get; set; }
+
+    public string? ImageUri { get; set; }
+    public List<Author>? Authors { get; set; }
+    public PriceData? Prices { get; set; }
+    public ContentData? Content { get; set; }
+    public string? Hours { get; set; }
+    public string? LikesInProcent { get; set; }
+    public string? Likes { get; set; }
+
+    public List<string>? Categories { get; set; } // Lägg till denna rad
+}
+
+public class Author
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
+public class PriceData
+{
+    public string? Currency { get; set; }
+    public double Price { get; set; }
+    public double Discount { get; set; }
+}
+public class ContentData
+{
+    public string? Description { get; set; }
+    public string[]? Includes { get; set; }
+    public virtual List<ProgramDetailItemData>? ProgramDetails { get; set; }
+}
+public class ProgramDetailItemData
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+}
---
+++ SiliconBlazor/SiliconBlazor/Models/FullCourseModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SiliconBlazor.Models;
+
+public class FullCourseModel
+{
+    public string? Id { get; set; }
+    public string? ImageUri { get; set; }
+    public string? ImageHeaderUri { get; set; }
+    public bool IsBestSeller { get; set; }
+    public bool IsDigital { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Ingress { get; set; }
+    public decimal StarRating { get; set; }
+    public string? Reviews { get; set; }
+    public List<Author>? Authors { get; set; }
+    public string? Article { get; set; }
+    public string? Resource { get; set; }
+    public bool? Access { get; set; }
+    public bool? Certificate { get; set; }
+    public string? Price { get; set; }
+    public string? DiscountPrice { get; set; }
+    public string? Currency { get; set; }
+
+    public string? Content { get; set; }
+    public string? Hours { get; set; }
+    public string? LikesInNumers { get; set; }
+    public string? LikesInProcent { get; set; }
+    public string? Description { get; set; }
+
+}
