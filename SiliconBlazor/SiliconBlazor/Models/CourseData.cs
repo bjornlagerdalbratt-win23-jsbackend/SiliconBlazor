dddbca5040121217b2aba8952c01e1d80e61d234
Helga Reesalu <helga.reesalu@gmail.com>
2024-05-30 10:18:20 +0200

    Added ImageUrl (Author)

--- SiliconBlazor/SiliconBlazor/Models/CourseData.cs
+++ SiliconBlazor/SiliconBlazor/Models/CourseData.cs
@@ -27,6 +27,7 @@ public class Author
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? ImageUrl { get; set; }
 }
 
 public class PriceData
