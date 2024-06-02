bd22d0f1d19ff6ef140ec02184f035597c043db0
Rebecca <rebeccalagerr@outlook.com>
2024-06-02 21:52:40 +0200

    Updated some code, done.

--- SiliconBlazor/SiliconBlazor/Models/SubscriberNotificationsModel.cs
+++ SiliconBlazor/SiliconBlazor/Models/SubscriberNotificationsModel.cs
@@ -4,7 +4,10 @@ namespace SiliconBlazor.Models;
 
 public class SubscriberNotificationsModel
 {
-    [Required]
+    [Required(ErrorMessage = "Enter a valid email address")]
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$", ErrorMessage = "Invalid e-mail address")]
     public string Email { get; set; } = null!;
     public bool DailyNewsletter { get; set; } = true;
     public bool AdvertisingUpdates { get; set; } = true;
--- SiliconBlazor/SiliconBlazor/ViewModels/ProfileInfoViewModel.cs
+++ SiliconBlazor/SiliconBlazor/ViewModels/ProfileInfoViewModel.cs
@@ -1,13 +1,31 @@
-﻿namespace SiliconBlazor.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiliconBlazor.ViewModels;
 
 public class ProfileInfoViewModel
 {
     public string UserProfileId { get; set; } = null!;
     public string? ProfileImage { get; set; }
+
+    [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "First name is required")]
+    [DataType(DataType.Text)]
     public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Last name is required")]
+    [DataType(DataType.Text)]
     public string LastName { get; set; } = null!;
+
     public string Email { get; set; } = null!;
+
+    [Display(Name = "Bio", Prompt = "Add a short bio...", Order = 4)]
+    [DataType(DataType.MultilineText)]
     public string? Bio { get; set; }
+
+    [Display(Name = "Phone", Prompt = "Enter your phone", Order = 3)]
+    [DataType(DataType.PhoneNumber)]
+    [Required(ErrorMessage = "Phone number not correct format")]
     public string? PhoneNumber { get; set; }
 
     public AddressViewModel? Address { get; set; }
